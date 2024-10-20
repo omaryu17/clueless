8f84e2ff96f12fb723c4a2ebcfbbf02b886ba2c7
omaryu17 <omaryu@berkeley.edu>
2024-10-19 23:00:19 -0400

    idk

--- server.py
+++ server.py
@@ -1,46 +1,55 @@
 import eventlet
 eventlet.monkey_patch()
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-# later replace * with actual client url once it's deployed
+# later replace * with actual client url once it"s deployed
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+# map client id (random hash) to player id (>= 0)
+clients = {}
+# count = 0
+
 # HTTP route for cron job to keep server alive
-@app.route('/ping', methods=['GET'])
+@app.route("/ping", methods=["GET"])
 def ping():
     with app.app_context():
         return jsonify({"response": "pong active"})
 
 # handle client connection
-@socketio.on('connect')
+@socketio.on("connect")
 def handle_connect():
     with app.app_context():
-        print('Client connected')
-        emit('server_message', {'response': 'Welcome to the game!'})
+        print(f"Client {request.sid} connected")
+        emit("server_message", {"response": "Welcome to the game!"}, to=request.sid)
+        emit("server_message", {"response": f"Player {request.sid} has joined."})
         print("emitted")
 
 # handle client disconnection
-@socketio.on('disconnect')
+@socketio.on("disconnect")
 def handle_disconnect():
     with app.app_context():
-        print('Client disconnected')
+        print(f"Client {request.sid} disconnected")
+        emit("server_message", {"response": f"Player {request.sid} has disconnected."})
+        print("emitted")
 
 # handle message from client, then broadcast to all connected clients
-@socketio.on('client_message')
+@socketio.on("client_message")
 def handle_client_message(data):
-    message = data['message']
-    print(f"Message from client: {message}")
-    emit('server_message', {'response': f"Broadcast: {message}"}, broadcast=True)
-    print("emitted")
+    with app.app_context():
+        message = data["message"]
+        print(f"Message from client {request.sid} : {message}")
+        emit("server_broadcast", {"response": f"Server Broadcast - Player {request.sid} sent: {message}"}, broadcast=True)
+        print("emitted")
 
 # TODO: ADD MORE MESSAGE HANDLERS FOR SPECIFIC ACTIONS
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
